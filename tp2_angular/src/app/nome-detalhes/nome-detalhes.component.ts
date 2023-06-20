112bbfa1a0eec303ab028fd531270b608cf02414
henriquefugie <henriquefugie@gmail.com>
2023-06-20 01:08:15 -0300

    initial commit

---
+++ tp2_angular/src/app/nome-detalhes/nome-detalhes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nome-detalhes',
+  templateUrl: './nome-detalhes.component.html',
+  styleUrls: ['./nome-detalhes.component.css']
+})
+export class NomeDetalhesComponent {
+
+}
